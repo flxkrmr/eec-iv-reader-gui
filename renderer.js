7a94627131bafe80a5ce1750d047a03c694cba1a
flxkrmr <felixkramerroki@aol.com>
2023-03-12 20:15:00 +0100

    Development

--- renderer.js
+++ renderer.js
@@ -1,15 +1,21 @@
-let keepReading = true;
-let reader;
-let writer;
-let closedPromise;
-
 const connectButton = document.getElementById('serialconnect');
+const alertBox = document.getElementById('alert');
+const initDataContainer = document.getElementById('init-data-container');
+const liveDataContainer = document.getElementById('live-data-container');
+
 connectButton.addEventListener('click',serialConnect);
+
+// XXX
+initDataContainer.style.display = 'none';
+liveDataContainer.style.display = 'flex';
 
 const serial = new Serial();
 
 async function serialConnect() {
+  connectButton.disabled = true;
+  alertBox.innerHTML="Connecting to EEC-IV-Reader..."; 
   console.log("Connecting");
+
   const ports = await serial.getPorts();
   // TODO check for ports, make select menu
   const port = ports[0];
@@ -17,12 +23,37 @@ async function serialConnect() {
   serial.connect(port, exception => {
     console.log("Connection error");
     console.log(exception);
+    alertBox.innerHTML="An error occured while connecting"; 
+    connectButton.disabled = false;
   }, () => {
     console.log("Connected");
   });
-}
 
-async function serialDisconnect() {
-  console.log("Disconnect");
+  serial.onReaderOnline = () => {
+    alertBox.innerHTML="EEC-IV-Reader connected!"; 
+  };
+
+  serial.onTimeoutError = () => {
+    alertBox.innerHTML="Reader could not connect to ECU. Is the ignition on?";
+  }
+
+  serial.onReadingStarted = type => {
+    console.log(type);
+    if (type == 0x01) {
+      alertBox.innerHTML="Fault Code Memory Reading started...";
+    } else if (type == 0x02) {
+      alertBox.innerHTML="KOEO/KOER Test started...";
+    } else if (type == 0x03) {
+      alertBox.innerHTML="Live Data Reading started...";
+    } else {
+      alertBox.innerHTML="Reading started";
+    }
+  }
+
+  serial.onLiveData = (data) => {
+    alertBox.innerHTML="Receiving live data";
+    initDataContainer.style.display = 'none';
+    liveDataContainer.style.display = 'flex';
+  }
 }
 
